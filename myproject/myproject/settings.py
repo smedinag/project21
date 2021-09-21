89887fade1d6343cd838ad4c81cfe413e7c14170
Susana <smedinag@usbcali.edu.co>
2021-09-21 13:02:12 -0500

    segundo commit con la BD

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pydb',
+        'USER': 'postgres',
+        'PASSWORD': 'blaumond',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
