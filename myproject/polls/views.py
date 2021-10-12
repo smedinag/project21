055e1f21890b4da82bb087c1e74ce497ef491e99
Susana <smedinag@usbcali.edu.co>
2021-10-12 13:05:35 -0500

    Mejoras a las vistas y error 404

--- myproject/polls/views.py
+++ myproject/polls/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import get_object_or_404
+from django.template import loader
 from .models import Question
 
 # Create your views here.
@@ -7,14 +10,35 @@ from .models import Question
 #def index(request):
 #    return HttpResponse("Hola, hoy es septiembre 21 de 2021. Estas en el index de polls!")
 
+#def index(request):
+#    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+#    output= ','.join([q.question_text for q in latest_question_list])
+#    return HttpResponse(output)
+##-------------------------
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output= ','.join([q.question_text for q in latest_question_list])
-    return HttpResponse(output)
 
-##-------------------------
+    #template = loader.get_template('polls/index.html') ## loader es necesario si se usa el HtppResponse
+    context= {
+        'latest_question_list': latest_question_list,
+    }
+    #return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
+
+#def detail(request, question_id):
+#    return HttpResponse('Estás viendo la pregunta # %s.' % question_id)
+
+#def detail(request, question_id):
+#   try:
+#        question = Question.objects.get(pk=question_id)
+#    except Question.DoesNotExist:
+#        raise Http404('Esta pregunta no existe!')
+#    return render(request, 'polls/detail.html', {'question': question})
+
 def detail(request, question_id):
-    return HttpResponse('Estás viendo la pregunta # %s.' % question_id)
+    question= get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
 
 def results(request, question_id):
     response= 'Estás viendo los resultados de la pregunta # %s'
